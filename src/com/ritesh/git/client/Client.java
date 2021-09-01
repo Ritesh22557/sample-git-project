fe520427a06514f95f5b03817231f86ed91d656d
Ritesh Singh <ritesh.singh22557@gmail.com>
2021-09-01 21:46:26 +0530

    Tested Successfully

--- src/com/ritesh/git/client/Client.java
+++ src/com/ritesh/git/client/Client.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import com.ritesh.git.Employee;
+import com.ritesh.git.comparaters.AgeComparater;
+import com.ritesh.git.comparaters.NameComparator;
 
 public class Client {
 
@@ -11,11 +13,11 @@ public class Client {
 
 		Employee e1 = new Employee(20,"Ritesh Singh");
 		
-		Employee e2 = new Employee(23,"Ritesh Singh");
+		Employee e2 = new Employee(23,"Aitesh Singh");
 		
-		Employee e3 = new Employee(10,"Ritesh Singh");
+		Employee e3 = new Employee(10,"Nitesh Singh");
 		
-		Employee e4 = new Employee(90,"Ritesh Singh");
+		Employee e4 = new Employee(90,"Sitesh Singh");
 		
 		ArrayList<Employee> al = new ArrayList<>();
 		al.add(e1);
@@ -23,7 +25,9 @@ public class Client {
 		al.add(e3);
 		al.add(e4);
 		// implemented comparable interface
-		Collections.sort(al);
+		
+		//Collections.sort(al);
+		Collections.sort(al,new NameComparator());
 		
 		System.out.println(al.toString());
 	}
