2317bdb2eb1925c2910d29093371f2b6043a7be2
Ritesh Singh <ritesh.singh22557@gmail.com>
2021-09-01 20:57:59 +0530

    Tested Successfully

--- src/com/ritesh/git/client/Client.java
+++ src/com/ritesh/git/client/Client.java
@@ -1,10 +1,30 @@
 package com.ritesh.git.client;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import com.ritesh.git.Employee;
+
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Employee e1 = new Employee(20,"Ritesh Singh");
+		
+		Employee e2 = new Employee(23,"Ritesh Singh");
+		
+		Employee e3 = new Employee(10,"Ritesh Singh");
+		
+		Employee e4 = new Employee(90,"Ritesh Singh");
+		
+		ArrayList<Employee> al = new ArrayList<>();
+		
+		al.add(e1);
+		al.add(e2);
+		al.add(e3);
+		al.add(e4);
+		Collections.sort(al);
+		System.out.println(al.toString());
 	}
 
 }
