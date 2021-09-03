d4b3bc9b7c872747d38e55fa4cbb482f01a3d51c
ns534 <ns534@LAPTOP-VSEJ831E.mshome.net>
2021-09-03 16:12:12 +0530

    Comments

--- src/com/ritesh/git/client/Client.java
+++ src/com/ritesh/git/client/Client.java
@@ -29,7 +29,7 @@ public class Client {
 		// implemented comparable interface
 		
 		//Collections.sort(al);
-		//Collections.sort(al,new AgeComparater());
+		Collections.sort(al,new AgeComparater());
 		Collections.sort(al,new NameComparator());
 		
 		System.out.println(al.toString());
